0a1cf1367d3bff22c7220b7835d6223c25fd8da5
mwongka <mikekawong@gmail.com>
2016-09-23 21:55:07 -0700

    create login API method

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -1,4 +1,6 @@
 //format 'userid%username'
+var User = require('../../db/models/user');
+var Link = require('../../db/models/link');
 
 module.exports = {
   // test route for Postman and Mocha TDD
@@ -9,12 +11,25 @@ module.exports = {
 
   // user Login or create new user API //
   login: function(req, res, next){
-    
+    var userID = req.body.userID;
+    var userName = req.body.name;
+    User.findOne({fbid: userID})
+      .then(function(user){
+        if(user){
+          res.sendStatus(200);
+        } else {
+          User.create({fbid: userID, fbname: userName})
+            .then(function(user){
+              console.log(userName + ' added to database');
+              res.sendStatus(201);
+            })
+        }
+      })
   },
 
   // user request API // 
   getLinks: function(req, res, next){
-
+    
   },
 
   putLinks: function(req, res, next){
