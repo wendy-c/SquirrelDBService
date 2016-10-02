396a82b8921d58f23b52d5151d45b6ee489e772a
mwongka <mikekawong@gmail.com>
2016-10-01 17:47:10 -0700

    search by descending order

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -42,9 +42,9 @@ module.exports = {
     const userID = req.params.userid;
     const promises = [];
 
-    Link.findAll({where:{
-      owner: userID,
-    }})
+    Link.findAll({where:{owner: userID,},
+      order: [['createdAt', 'DESC']],
+    })
     .then(function(data){
       console.log('please work', data)
       const mapped = data.map(function(curr){
@@ -220,7 +220,7 @@ module.exports = {
     Link.create({url:url, owner:friendID, assignee:userID})
     .then(function(link){
       console.log('You added a link for your friend!');
-      res.sendStatus(201);
+      res.send(link).sendStatus(201);
     })
   },
 }
